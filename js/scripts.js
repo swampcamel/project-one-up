9865ea35b2b19d10c4448fc8916552f09766df9f
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-08-27 10:40:31 -0700

    begin fight mechanic

--- js/scripts.js
+++ js/scripts.js
@@ -18,19 +18,25 @@ function Player(input) {
   this.hp = 10;
 };
 
+function Card() {
+  this.power = 1;
+  this.health = 1;
+  this.name = "";
+}
+
 Game.prototype.startGame(input1, input2) { //inputs 1 and 2 are entered player names
   var player1 = new Player(input1);
   var player2 = new Player(input2);
   var newBoard = new Board();
 };
 
-Board.prototype.monsterFight(cardObj1, cardObj2, board) {
-  cardObj1.health = cardObj1.health - cardObj2.attack;
-  cardObj2.health = cardObj2.health - cardObj1.attack;
-  if (cardObj1.health <= 0) {
-    move cardObj1 graveyard;
-  }
-  if (cardObj2.health <= 0) {
-    move cardObj1 geraveyard;
-  }
-};
+// Board.prototype.monsterFight(cardObj1, cardObj2, board) {
+//   cardObj1.health = cardObj1.health - cardObj2.power;
+//   cardObj2.health = cardObj2.health - cardObj1.power;
+//   if (cardObj1.health <= 0) {
+//     move cardObj1 graveyard;
+//   }
+//   if (cardObj2.health <= 0) {
+//     move cardObj1 geraveyard;
+//   }
+// };
