91a37ce312271817f9757576eaef80c0c757c9fa
Ben Kiggen and Josh Gearheart <josh@>
2018-08-29 15:02:19 -0700

    add glow to player info

--- js/scripts.js
+++ js/scripts.js
@@ -291,16 +291,18 @@ $(document).on('click', '.board-lanes', function() {
     handIndexofCard = handIndexofCard[2];
     console.log(handIndexofCard);
     $(".active-card").removeClass("active-card");
+
     if (newGame.activePlayer == 1) {
       var activeCard = newGame.board.p1Hand.splice(handIndexofCard, 1);
       var activeCard = activeCard[0];
       var index1 = 0;
       $('#player-1-hand').empty();
+
       newGame.board.p1Hand.forEach(function(card) {
         $('#player-1-hand').append('<div id=\"p1' + index1 +'\" class=\"hand-cards\"><img src=\"img/card-frame_180-res-alt.png\"></div>');
         index1++;
-      newGame.board.p1Field[boardIndex] = activeCard;
       });
+      newGame.board.p1Field[boardIndex] = activeCard;
 
     } else if (newGame.activePlayer == 2) {
       var activeCard = newGame.board.p2Hand.splice(handIndexofCard, 1);
@@ -311,6 +313,7 @@ $(document).on('click', '.board-lanes', function() {
         $('#player-2-hand').append('<div id=\"p2' + index2 +'\"  class=\"hand-cards\"><img src=\"img/card-frame_180-res-alt.png\"></div>'); //previously "p1' + index
         index2++; //previously index1
       });
+
       newGame.board.p2Field[boardIndex] = activeCard;
 
     } else {
