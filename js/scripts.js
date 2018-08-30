06e554b981df5b47106b1046c1e5d3f0074090a5
GrumpyEwok <evanfilkins@gmail.com>
2018-08-30 13:43:02 -0700

    Add form to landing page. Animate.

--- js/scripts.js
+++ js/scripts.js
@@ -255,6 +255,30 @@ function showHandCards(gameObj) {
 //begin user interface
 $(document).ready(function(){
 
+  $("#logo").addClass("slide-in-blurred-tr");
+
+    $("#logo").click(function() {
+      $("#logo").addClass("slide-out-bl");
+      $("#banner").show().addClass("slide-in-top");
+      $("#play-button").show().addClass("fade-in-fwd")
+    });
+
+    $("#play-button").click(function() {
+      $("#banner").addClass("fade-out");
+      $("#banner-blank").show();
+      $("#names-input").show();
+      $("#play-button").hide();
+      $("#confirm-button").show();
+    })
+
+    $("#confirm-button").click(function() {
+      $("form").submit();
+      event.preventDefault();
+      alert("Wasdasds");
+    })
+
+
+
   $("#new-game").click(function() {
       newGame = new Game();
       newGame.startGame();
